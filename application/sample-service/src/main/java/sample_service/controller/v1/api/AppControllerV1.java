bd2299f2e5df18e06cfda826198495291c983cb1
Nadeem Khan <nadeem4.nk13@gmail.com>
2020-11-24 22:39:24 +0530

    updated readme with how to create new service

--- application/sample-service/src/main/java/sample_service/controller/v1/api/AppControllerV1.java
+++ application/sample-service/src/main/java/sample_service/controller/v1/api/AppControllerV1.java
@@ -3,6 +3,7 @@ package sample_service.controller.v1.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
